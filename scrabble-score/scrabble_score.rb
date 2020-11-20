9d05fcac5d1386e094e7401bf8ea3a2429061448
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-11-20 21:34:55 +0100

    rafacto scrabble_score

--- scrabble-score/scrabble_score.rb
+++ scrabble-score/scrabble_score.rb
@@ -2,42 +2,31 @@
 
 # give score for a word on Scrabble game
 class Scrabble
-  TABLE_SCORE = {
-    A: 1,
-    B: 3,
-    C: 3,
-    D: 2,
-    E: 1,
-    F: 4,
-    G: 2,
-    H: 4,
-    I: 1,
-    J: 8,
-    K: 5,
-    L: 1,
-    M: 3,
-    N: 1,
-    O: 1,
-    P: 3,
-    Q: 10,
-    R: 1,
-    S: 1,
-    T: 1,
-    U: 1,
-    V: 4,
-    W: 4,
-    X: 8,
-    Y: 4,
-    Z: 10
-  }.freeze
+  attr_reader :letters
+
+  TILES = Hash.new(0).update({
+                               A: 1, N: 1,
+                               B: 3, O: 1,
+                               C: 3, P: 3,
+                               D: 2, Q: 10,
+                               E: 1, R: 1,
+                               F: 4, S: 1,
+                               G: 2, T: 1,
+                               H: 4, U: 1,
+                               I: 1, V: 4,
+                               J: 8, W: 4,
+                               K: 5, X: 8,
+                               L: 1, Y: 4,
+                               M: 3, Z: 10
+                             }).freeze
 
   def initialize(word)
-    @word = word || ''
+    @letters = word.to_s.upcase.chars.map(&:to_sym)
   end
 
   def score
-    @word.upcase.chars.sum do |l|
-      TABLE_SCORE[l.to_sym] || 0
+    letters.sum do |l|
+      TILES[l]
     end
   end
 
