80ed18b4704ab600826e38cc0521e915291b2289
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-11-21 20:56:21 +0100

    another rafacto of scrabble-score

--- scrabble-score/scrabble_score.rb
+++ scrabble-score/scrabble_score.rb
@@ -1,20 +1,20 @@
 # give score for a word on Scrabble game
 class Scrabble
-  LATIN_TILES = Hash.new(0).update({
-                                     A: 1, N: 1,
-                                     B: 3, O: 1,
-                                     C: 3, P: 3,
-                                     D: 2, Q: 10,
-                                     E: 1, R: 1,
-                                     F: 4, S: 1,
-                                     G: 2, T: 1,
-                                     H: 4, U: 1,
-                                     I: 1, V: 4,
-                                     J: 8, W: 4,
-                                     K: 5, X: 8,
-                                     L: 1, Y: 4,
-                                     M: 3, Z: 10
-                                   }).freeze
+  TILES = {
+    A: 1, N: 1,
+    B: 3, O: 1,
+    C: 3, P: 3,
+    D: 2, Q: 10,
+    E: 1, R: 1,
+    F: 4, S: 1,
+    G: 2, T: 1,
+    H: 4, U: 1,
+    I: 1, V: 4,
+    J: 8, W: 4,
+    K: 5, X: 8,
+    L: 1, Y: 4,
+    M: 3, Z: 10
+  }.freeze
 
   def self.score(tiles)
     new(tiles).score
@@ -24,9 +24,11 @@ class Scrabble
 
   attr_reader :letters, :tiles
 
-  def initialize(word, tiles = LATIN_TILES)
+  def initialize(word, tiles = TILES)
     @letters = word.to_s.upcase.chars.map(&:to_sym)
-    @tiles = tiles
+    t = tiles.dup
+    t.default = 0
+    @tiles = t.freeze
   end
 
   public
